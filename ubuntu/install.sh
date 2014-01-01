1a7466f0212700e1ad7813a92e3666f065236403
Johnny.Lv <kkiiiu@gmail.com>
2014-01-01 23:18:10 +0800

    [LV] - ubuntu env

---
+++ ubuntu/install.sh
@@ -0,0 +1,15 @@
+# - Update apt-get
+sudo apt-get update
+sudo apt-get upgrade 
+
+# - Install zsh
+sudo apt-get install zsh
+
+# - Enhance
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh 
+
+# - Set zsh as default shell
+chsh -s /bin/zsh
+
+# - Install git
+sudo apt-get install git
