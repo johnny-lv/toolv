0c5e3c7452d0bd3a23b505c7b0c95ff7721ee73d
Johnny.Lv <kkiiiu@gmail.com>
2014-01-02 12:15:42 +0800

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,15 +1,24 @@
 # - Update apt-get
 sudo apt-get update
-sudo apt-get upgrade 
+sudo apt-get upgrade
 
-# - Install zsh
-sudo apt-get install zsh
+installZSH();
+installGit();
 
-# - Enhance
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh 
 
-# - Set zsh as default shell
-chsh -s /bin/zsh
+## Install zsh
+function installZSH() {
+  echo "## Start installing zsh..."
+  sudo apt-get install zsh
+  wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh 
+  # - Set zsh as default shell
+  chsh -s /bin/zsh
+  echo "## Installed successfully."
+}
 
-# - Install git
-sudo apt-get install git
+## Install git
+function installGit() {
+  echo "## Start installing git..."
+  sudo apt-get install git
+  echo "## Installed successfully."
+}
