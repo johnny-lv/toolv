0648a8b9d24fe47186b05291cbc40a664272ea65
Johnny.Lv <kkiiiu@gmail.com>
2014-01-04 22:57:30 +0800

    [JL] - Shell auto

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,15 +1,44 @@
-# - Update apt-get
-sudo apt-get update
-sudo apt-get upgrade 
+#!/bin/bash
+
+# - Install Misc
+installMisc() {
+    echo "Start installing Misc tools..." 
+    sudo apt-get install curl
+    sudo apt-get install openssh-server
+    sudo apt-get install vim
+    echo "Misc tools installed successfully!" 
+}
 
 # - Install zsh
-sudo apt-get install zsh
-
-# - Enhance
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh 
-
-# - Set zsh as default shell
-chsh -s /bin/zsh
+installZsh() {
+    echo "Start installing zsh..." 
+    sudo apt-get install zsh
+    # - Enhance
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh 
+    # - Set zsh as default shell
+    chsh -s /bin/zsh
+    echo "zsh installed successfully!" 
+}
 
 # - Install git
-sudo apt-get install git
+installGit() {
+    echo "Start installing git..." 
+    sudo apt-get install git-core
+    echo "Git installed successfully!" 
+}
+
+
+# - Update apt-get
+sudo apt-get update
+sudo apt-get upgrade
+
+read -p "Ready to install?" RESP
+if ["$RESP" = "y"]; then
+    echo ">>>>>>>>>>>>Now begin!"
+    #installGit
+    #installZsh
+    #installMisc
+    #installNode
+else
+    echo "Abort installation!"
+fi
